7b8d32b98d0279bf389608d56b3866101349f6a7
CloverX70 <kamal.tabaja12@gmail.com>
2026-08-05 06:45:04 +0300

    display shared posts in group messages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["elitemessenger.s3.eu-north-1.amazonaws.com", "api.dicebear.com"],
+    domains: [
+      "elitemessenger.s3.eu-north-1.amazonaws.com",
+      "api.dicebear.com",
+      "lh3.googleusercontent.com",
+    ],
 
     remotePatterns: [
       {
